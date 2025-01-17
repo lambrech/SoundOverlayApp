120a28f1e590c14612bbbccedf558904c96aaf54
Christian Lambrecht <chris.lambrecht@web.de>
2025-01-17 18:06:33 +0100

    very very first ugly version

---
+++ SoundOverlayApp/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace SoundOverlayApp;
+
+public class Extensions
+{
+    public static (string version, string buildConfig, string commitId, string additionalCommitInfo, string fullVersionText) GetVersionInformation()
+    {
+        var attributes = Assembly.GetEntryAssembly()?.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false);
+        Debug.Assert(attributes != null, nameof(attributes) + " != null");
+        var fullVersionText = attributes.Length == 0
+            ? $"Error getting Informtional Version ... {Assembly.GetEntryAssembly()?.GetName().Version}"
+            : ((AssemblyInformationalVersionAttribute)attributes[0]).InformationalVersion;
+
+        var versionParts = fullVersionText.Split('|').Select(x => x.Trim()).ToList();
+
+        if (versionParts.Count < 3)
+        {
+            return (fullVersionText, $"Found only {versionParts.Count} parts.", fullVersionText, fullVersionText, fullVersionText);
+        }
+
+        var version = versionParts[0];
+        var buildConfig = versionParts[1].Replace(@"configName:", string.Empty);
+
+        var isDirty = versionParts[2].Contains(@"-dirty");
+        var tmpCommitId = versionParts[2].Replace(@"git:", string.Empty);
+
+        if (isDirty)
+        {
+            tmpCommitId = tmpCommitId.Replace(@"-dirty", string.Empty);
+        }
+
+        var commitId = tmpCommitId.Substring(tmpCommitId.Length - 40, 40);
+        var additionalCommitInfo = $"{(isDirty ? "CAUTION! UNOFFICIAL/DIRTY BUILD VERSION!!! " : string.Empty)}{versionParts[3]}";
+
+        return (version, buildConfig, commitId, additionalCommitInfo, fullVersionText);
+    }
+}
