9e6cc841d0f584fab0ade4acd5996fcd223b6734
Christian Lambrecht <chris.lambrecht@web.de>
2025-01-23 03:40:40 +0100

    start adding some kind of model

---
+++ SoundOverlayApp/Model/Records.cs
@@ -0,0 +1,8 @@
+﻿namespace SoundOverlayApp.Model;
+
+public record SoundBase(string DisplayName);
+
+public record StoredSound(string DisplayName, string StoredPath) : SoundBase(DisplayName);
+
+public record SilenceSound(string DisplayName, TimeSpan Duration, bool WithHalfTimeBeep) : SoundBase(DisplayName);
+
